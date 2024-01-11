9136387590c7513d80ca65a639834ebc0fbe04f6
mahibashardeveloper <mahibashar2023@gmail.com>
2024-01-11 23:30:33 +0600

    update issues

--- resources/js/front/pages/profile/layout/layout.vue
+++ resources/js/front/pages/profile/layout/layout.vue
@@ -5,41 +5,41 @@
             <div class="d-flex justify-content-center">
                 <label for="avatar" class="form-label customer-avatar" v-if="!url">
                     <input id="avatar" type="file" name="avatar" class="d-none" @change="onFileChange">
-                    <i class="bi bi-camera fs-1"></i>
+                    <i class="bi bi-camera fs-1 text-theme"></i>
                 </label>
                 <div class="customer-avatar" v-if="url">
                     <img :src="url" alt="avatar">
                     <div class="delete-icon" @click="url = null">
-                        <i class="bi bi-trash"></i>
+                        <i class="bi bi-trash2-fill text-danger"></i>
                     </div>
                 </div>
             </div>
-            <div class="text-center mt-3">
+            <div class="text-center mt-4 fs-4">
                 <a href="javascript:void(0)" class="text-theme text-decoration-none">Edit Profile</a>
             </div>
             <div class="pt-4">
                 <div class="mb-3 fw-semibold">
                     Full Name:
                 </div>
-                <div class="mb-3">
+                <div class="mb-3 text-secondary">
                     Mahi Bashar Akash
                 </div>
                 <div class="mb-3 fw-semibold">
                     Email:
                 </div>
-                <div class="mb-3">
+                <div class="mb-3 text-secondary">
                     mahibashar2023@gmail.com
                 </div>
                 <div class="mb-3 fw-semibold">
                     Phone Number:
                 </div>
-                <div class="mb-3">
+                <div class="mb-3 text-secondary">
                     +880 01645820007
                 </div>
                 <div class="mb-3 fw-semibold">
                     Gender
                 </div>
-                <div class="mb-3">
+                <div class="text-secondary">
                     Male
                 </div>
             </div>
