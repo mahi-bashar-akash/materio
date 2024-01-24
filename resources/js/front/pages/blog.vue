a868377c4e0e4c50181b2be5f311bb8f60036164
mahibashardeveloper <mahibashar2023@gmail.com>
2024-01-24 16:26:44 +0600

    update changes

--- resources/js/front/pages/blog.vue
+++ resources/js/front/pages/blog.vue
@@ -1,60 +1,44 @@
 <template>
 
-    <div class="container-fluid cursor-content-menu mb-4">
-        <img :src="`/images/slider/banner-1.jpg`" class="img-fluid object-fit-cover w-100" style="height: 450px;" alt="blog 1">
+    <div class="container-fluid cursor-pointer-menu mb-4 blogs" v-if="singleData === null">
+        <div class="row">
+            <div class="col-md-6 col-lg-4 p-3 mb-3" v-for="each in blogList">
+                <div class="bg-white blog-content rounded-3 p-3 cursor-pointer" @click="showInfo(each)">
+                    <div class="mb-3 overflow-hidden">
+                        <img :src="each.file_path" class="img-fluid w-100 object-fit-cover image-effect" alt="blog-1">
+                    </div>
+                    <div class="fs-4 fw-semibold mb-3 text-theme">
+                        {{each.title}}
+                    </div>
+                    <div class="mb-3">
+                        {{each.description}}
+                    </div>
+                    <hr class="border">
+                    <div class="text-secondary">
+                        {{each.date}} | <span class="text-theme"> By {{each.author}} </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid cursor-content-menu mb-4" v-if="singleData !== null">
+        <div class="mb-3">
+            <button type="button" class="btn btn-outline-theme" @click="singleData = null">
+                <i class="bi bi-chevron-left"></i>
+            </button>
+        </div>
+        <img :src="singleData.file_path" class="img-fluid object-fit-cover w-100" style="height: 450px;" :alt="singleData.title">
         <div class="my-3 text-theme fw-semibold fs-4">
-            Blog title
+            {{singleData.title}}
         </div>
-        <div class="fw-semibold">
-            Mahi Bashar Akash
+
+        <div class="text-secondary mb-3">
+            {{singleData.date}} | <span class="text-theme"> By {{singleData.author}} </span>
         </div>
-        <small class="d-block mb-3 text-secondary text-opacity-50">
-            june 18, 2023
-        </small>
+
         <div class="text-secondary text-opacity-50 fw-light mb-3">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque aut beatae blanditiis consequatur delectus
-            deserunt dolor dolorem earum id, illum incidunt minus natus nemo officia omnis, praesentium quae quaerat
-            <br><br>
-            quasi quis quos rem unde vitae! Eos, necessitatibus, odit! Accusantium, animi assumenda at consequatur
-            cumque earum, eligendi eos est et hic incidunt ipsam iusto magni molestiae necessitatibus quae, quam quidem
-            quo rem sed sit ullam voluptas? Aperiam atque mollitia omnis ut. Alias cupiditate dolor dolores dolorum
-            earum eius eligendi, error fugit id illum ipsa labore libero nobis porro quaerat quidem quis sed similique
-            sint soluta suscipit velit vero? Aliquid dolor ducimus error eum harum, illum in ipsum nisi obcaecati
-            <br><br>
-            officiis repudiandae sapiente, veniam voluptate! At consequuntur laboriosam necessitatibus neque ratione.
-            Accusamus ad aut, beatae consectetur corporis delectus est et facilis fuga harum ipsa maxime minima,
-            qui repellat saepe sint sit soluta ullam veniam voluptatem. Ab amet aspernatur beatae consectetur
-            dolor doloremque dolores, dolorum eaque facere libero molestias nostrum officia pariatur provident quae
-            quasi similique tempora temporibus vero voluptas? Consectetur cupiditate dolorem ipsam magnam magni
-            neque nulla placeat ratione suscipit veritatis. Accusamus animi aspernatur atque autem consequuntur
-            <br><br>
-            cum cumque error eum ex ipsam labore libero nam, neque quos recusandae, temporibus velit voluptate
-            voluptatem? Alias architecto atque blanditiis cum deserunt distinctio doloremque doloribus earum eligendi
-            error est et eveniet facere illo ipsum iusto labore laborum magnam, molestias odit optio perferendis
-            praesentium, provident quae quam quasi quis recusandae reiciendis repudiandae sapiente sequi temporibus
-            unde veniam veritatis vero vitae voluptates! Ad aliquam aspernatur autem dolorem eaque fuga, hic minus
-            necessitatibus nesciunt quis ratione, repellendus reprehenderit rerum sed sint tenetur vero. Accusamus
-            ad aut dolorem fugiat hic inventore ipsa iste maiores maxime nemo officiis quae quam quibusdam repellendus
-            saepe, sed sequi sit, tempora tenetur ullam velit vero voluptatem! Beatae earum est fugit inventore
-            ipsam iure, mollitia neque saepe totam veniam! Beatae earum error esse eveniet harum laborum repellendus
-            <br><br>
-            voluptates voluptatibus. Neque, sed voluptatem! Aliquam corporis facere itaque molestiae quibusdam
-            tempore! Alias architecto at blanditiis commodi doloremque id illo necessitatibus, nulla officia quod
-            repudiandae suscipit velit vitae. Accusamus, at beatae eius enim eum exercitationem explicabo laborum
-            libero maxime minima neque perferendis perspiciatis porro ratione, sunt, totam ullam voluptas? Assumenda
-            <br><br>
-            atque consequuntur dolorem doloribus ex fuga laboriosam magni, mollitia non officia perspiciatis placeat
-            quae rem repellat sequi sint voluptatem. Ab alias aliquid amet asperiores assumenda consequatur deleniti
-            dicta error eveniet expedita explicabo illum ipsa itaque labore laudantium magni maiores maxime natus
-            nemo, nesciunt non omnis placeat porro possimus quibusdam quo ratione repellendus sequi sunt, suscipit
-            <br><br>
-            tenetur totam ut vero vitae voluptate voluptatem voluptatibus? Ab amet at atque, consequatur consequuntur
-            deserunt eaque enim ex exercitationem iusto necessitatibus nihil obcaecati omnis porro totam! Esse hic
-            impedit inventore maxime pariatur, perferendis quia sunt voluptatibus? Accusamus adipisci aliquam aut
-            <br><br>
-            autem deleniti distinctio dolore doloribus ducimus enim ex harum id impedit magnam magni modi natus, nihil
-            nostrum numquam odio praesentium quaerat quia quibusdam quis quod sit sunt, voluptas voluptatem. Cumque
-            fugiat inventore neque nihil quia. Autem consectetur doloremque dolores esse maxime non vitae.
+            {{singleData.description}}
         </div>
         <div class="d-flex justify-content-start align-items-center mt-4">
             <button type="button" class="btn btn-light me-2">
@@ -97,7 +81,15 @@ export default {
     data(){
 
         return{
+            blogList: [
+                { id: '1', file_path: '/images/blog/blog-6.jpg', title: 'All time fresh every time healthy', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit doli. Aenean commodo ligula eget dolor. ...', date: 'May 13, 2023', author: 'Spacing Tech' },
+                { id: '2', file_path: '/images/blog/blog-5.jpg', title: 'All time fresh every time healthy', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit doli. Aenean commodo ligula eget dolor. ...', date: 'May 13, 2023', author: 'Spacing Tech' },
+                { id: '3', file_path: '/images/blog/blog-4.jpg', title: 'All time fresh every time healthy', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit doli. Aenean commodo ligula eget dolor. ...', date: 'May 13, 2023', author: 'Spacing Tech' },
+                { id: '4', file_path: '/images/blog/blog-3.jpg', title: 'All time fresh every time healthy', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit doli. Aenean commodo ligula eget dolor. ...', date: 'May 13, 2023', author: 'Spacing Tech' },
+                { id: '5', file_path: '/images/blog/blog-2.jpg', title: 'All time fresh every time healthy', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit doli. Aenean commodo ligula eget dolor. ...', date: 'May 13, 2023', author: 'Spacing Tech' },
+            ],
 
+            singleData: null,
         }
 
     },
@@ -110,7 +102,9 @@ export default {
 
     methods: {
 
-
+        showInfo(data){
+            this.singleData = data
+        }
 
     }
 
