da2ffbbde11c69d556695c49ebf2b6f6808a583f
mahibashardeveloper <mahibashar2023@gmail.com>
2024-01-25 21:22:48 +0600

    update seller dashboard

--- resources/js/seller/pages/dashboard.vue
+++ resources/js/seller/pages/dashboard.vue
@@ -1,5 +1,81 @@
 <template>
 
+    <section class="w-100 bg-white">
+        <div class="container-fluid">
+            <div class="row">
+
+                <!-- sliders -->
+                <div class="col-md-6 p-3">
+                    <div class="border p-3 rounded-3">
+                        <div class="row align-items-center">
+                            <div class="col-6 fw-semibold d-flex align-items-center">
+                                <i class="bi bi-sliders py-2 px-3 shadow-sm d-inline-block me-2"></i>
+                                <div class="ps-2">
+                                    Sliders
+                                </div>
+                            </div>
+                            <div class="col-6 text-end">
+                                10,000,000
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- categories -->
+                <div class="col-md-6 p-3">
+                    <div class="border p-3 rounded-3">
+                        <div class="row align-items-center">
+                            <div class="col-6 fw-semibold d-flex align-items-center">
+                                <i class="bi bi-link-45deg py-2 px-3 shadow-sm d-inline-block me-2"></i>
+                                <div class="ps-2">
+                                    Categories
+                                </div>
+                            </div>
+                            <div class="col-6 text-end">
+                                10,000,000
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- products -->
+                <div class="col-md-6 p-3">
+                    <div class="border p-3 rounded-3">
+                        <div class="row align-items-center">
+                            <div class="col-6 fw-semibold d-flex align-items-center">
+                                <i class="bi bi-bag py-2 px-3 shadow-sm d-inline-block me-2"></i>
+                                <div class="ps-2">
+                                    Products
+                                </div>
+                            </div>
+                            <div class="col-6 text-end">
+                                10,000,000
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- orders -->
+                <div class="col-md-6 p-3">
+                    <div class="border p-3 rounded-3">
+                        <div class="row align-items-center">
+                            <div class="col-6 fw-semibold d-flex align-items-center">
+                                <i class="bi bi-cart-fill py-2 px-3 shadow-sm d-inline-block me-2"></i>
+                                <div class="ps-2">
+                                    Orders
+                                </div>
+                            </div>
+                            <div class="col-6 text-end">
+                                10,000,000
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
 </template>
 
 <script>
