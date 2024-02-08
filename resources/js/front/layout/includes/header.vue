e4922f6834665cdb76f07b73ae55b55615819a92
mahibashardeveloper <mahibashar2023@gmail.com>
2024-02-08 21:37:07 +0600

    update issues

--- resources/js/front/layout/includes/header.vue
+++ resources/js/front/layout/includes/header.vue
@@ -1,30 +1,12 @@
 <template>
 
-    <!-- header logo search some link -->
-    <div class="w-100 bg-white d-none d-xl-inline-block">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-2 col-4 p-3 p-lg-4">
-                    <router-link :to="{name: 'home'}" class="fw-bold fs-3 text-decoration-none text-dark">
-                        {{app_name}}
-                    </router-link>
-                </div>
-                <div class="col-lg-10 p-3 p-lg-4">
-                    <div class="position-relative">
-                        <input type="text" name="keyword" class="form-control ps-5" placeholder="Search Here" required autocomplete="new-search">
-                        <div class="position-absolute top-50 start-0 translate-middle-y ps-3">
-                            <i class="bi bi-search"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="bg-white w-100 sticky-top">
-        <nav class="navbar navbar-expand-xl container px-2 py-2 py-lg-4">
+    <header class="sticky-top w-100 bg-light p-3">
+        <nav class="navbar navbar-expand-lg container">
             <div class="container-fluid">
-                <router-link class="navbar-brand fw-bold d-inline-block d-xl-none fs-3" :to="{name: 'home'}">Materio</router-link>
-                <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <router-link class="navbar-brand" :to="{name: 'home'}">
+                    Materio
+                </router-link>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -36,133 +18,71 @@
                         </li>
                         <li class="nav-item">
                             <router-link class="nav-link" :to="{name: 'about'}">
-                                About Us
+                                About
                             </router-link>
                         </li>
                         <li class="nav-item">
-                            <router-link class="nav-link" :to="{name: 'contact'}">
-                                Contact Us
-                            </router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link class="nav-link" :to="{name: 'shop'}">
-                                Shop
+                            <router-link class="nav-link" :to="{name: 'about'}">
+                                Contact
                             </router-link>
                         </li>
                         <li class="nav-item">
                             <router-link class="nav-link" :to="{name: 'blog'}">
                                 Blog
-                                </router-link>
-                            </li>
-                            <li class="nav-item d-inline-block d-xl-none">
-                                <a class="nav-link" href="/auth/login">
-                                    Become a seller
-                                </a>
-                            </li>
-                            <li class="nav-item d-inline-block d-xl-none">
-                                <a class="nav-link" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
-                                    cart
-                                    <span class="badge bg-theme text-white">
-                                    {{products.length}}
+                            </router-link>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+                                Cart
+                                <span class="badge bg-theme text-white ms-2">
+                                    0
                                 </span>
-                                </a>
-                            </li>
-                            <li class="nav-item dropdown d-inline-block d-xl-none mb-2">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Mahi Bashar Akash
-                                </a>
-                                <ul class="dropdown-menu dropdown-menu-end p-0 mt-2 overflow-hidden">
-                                    <li v-if="!userInfo">
-                                        <a href="/auth/login" class="dropdown-item px-3 py-2">
-                                            Login
-                                        </a>
-                                    </li>
-                                    <li v-if="!userInfo">
-                                        <a href="/auth/register" class="dropdown-item px-3 py-2">
-                                            Registration
-                                        </a>
-                                    </li>
-                                    <li v-if="userInfo">
-                                        <router-link :to="{name: 'dashboard'}" class="dropdown-item px-3 py-2" @click="windowContent">
-                                            Dashboard
-                                        </router-link>
-                                    </li>
-                                    <li v-if="userInfo">
-                                        <router-link :to="{name: 'productReview'}" class="dropdown-item px-3 py-2" @click="windowContent">
-                                            Product Review
-                                        </router-link>
-                                    </li>
-                                    <li v-if="userInfo">
-                                        <router-link :to="{name: 'settings'}" class="dropdown-item px-3 py-2" @click="windowContent">
-                                            Settings
-                                        </router-link>
-                                    </li>
-                                    <li v-if="userInfo">
-                                        <button type="button" class="dropdown-item px-3 py-2" @click="windowContent">
-                                            Logout
-                                        </button>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <form class="d-flex d-xl-none" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success" type="submit">Search</button>
-                        </form>
-                        <ul class="navbar-nav d-none d-xl-flex">
-                            <div class="d-flex align-items-center justify-content-start">
-                                <a href="/auth/login" class="btn bg-white border me-3 d-flex justify-content-center align-items-center">
-                                    Become a seller <i class="bi bi-arrow-right-short ms-2"></i>
-                                </a>
-                                <button type="button" class="btn bg-white border position-relative me-3" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
-                                    <i class="bi bi-cart-fill"></i>
-                                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-secondary">
-                                {{products.length}}
-                            </span>
-                                </button>
-                                <div class="dropdown">
-                                    <button type="button" class="btn bg-white border" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        <i class="bi bi-person-fill"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item dropdown mb-2">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Mahi Bashar Akash
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end p-0 mt-2 overflow-hidden">
+                                <li v-if="!userInfo">
+                                    <a href="/auth/login" class="dropdown-item px-3 py-2">
+                                        Login
+                                    </a>
+                                </li>
+                                <li v-if="!userInfo">
+                                    <a href="/auth/register" class="dropdown-item px-3 py-2">
+                                        Registration
+                                    </a>
+                                </li>
+                                <li v-if="userInfo">
+                                    <router-link :to="{name: 'dashboard'}" class="dropdown-item px-3 py-2" @click="windowContent">
+                                        Dashboard
+                                    </router-link>
+                                </li>
+                                <li v-if="userInfo">
+                                    <router-link :to="{name: 'productReview'}" class="dropdown-item px-3 py-2" @click="windowContent">
+                                        Product Review
+                                    </router-link>
+                                </li>
+                                <li v-if="userInfo">
+                                    <router-link :to="{name: 'settings'}" class="dropdown-item px-3 py-2" @click="windowContent">
+                                        Settings
+                                    </router-link>
+                                </li>
+                                <li v-if="userInfo">
+                                    <button type="button" class="dropdown-item px-3 py-2" @click="windowContent">
+                                        Logout
                                     </button>
-                                    <ul class="dropdown-menu dropdown-menu-end p-0 mt-2 overflow-hidden shadow border-0">
-                                        <li v-if="!userInfo">
-                                            <a href="/auth/login" class="dropdown-item px-3 py-2">
-                                                Login
-                                            </a>
-                                        </li>
-                                        <li v-if="!userInfo">
-                                            <a href="/auth/register" class="dropdown-item px-3 py-2">
-                                                Registration
-                                            </a>
-                                        </li>
-                                        <li v-if="userInfo">
-                                            <router-link :to="{name: 'dashboard'}" class="dropdown-item px-3 py-2" @click="windowContent">
-                                                Dashboard
-                                            </router-link>
-                                        </li>
-                                        <li v-if="userInfo">
-                                            <router-link :to="{name: 'productReview'}" class="dropdown-item px-3 py-2" @click="windowContent">
-                                                Product Review
-                                            </router-link>
-                                        </li>
-                                        <li v-if="userInfo">
-                                            <router-link :to="{name: 'settings'}" class="dropdown-item px-3 py-2" @click="windowContent">
-                                                Settings
-                                            </router-link>
-                                        </li>
-                                        <li v-if="userInfo">
-                                            <button type="button" class="dropdown-item px-3 py-2" @click="windowContent">
-                                                Logout
-                                            </button>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </ul>
-                    </div>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
+    </header>
 
     <!-- right sidebar as offcanvas -->
     <div class="offcanvas offcanvas-end p-3 cursor-content-menu" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
