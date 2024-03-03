d5a64a76e4f782c1f8163c4cae83855f038cd133
mahibashardeveloper <mahibashar2023@gmail.com>
2024-03-03 22:09:01 +0600

    update changes

--- resources/js/front/layout/layout.vue
+++ resources/js/front/layout/layout.vue
@@ -1,35 +1,34 @@
 <template>
 
-    <div class="news-letter" :class="{ 'active': showNewsLetter }">
-        <div class="news-modal py-4 px-3 img-fluid object-fit-cover" :style="{ background: 'url(' + NewsLetterImageUrl + ')' }">
-
-            <div class="d-flex justify-content-end">
-                <button type="button" class="btn-close shadow-none" @click="closeNewsLetter"></button>
-            </div>
-
-            <div class="fs-5 text-center fw-bold mb-3 pt-5">
-                Become a subscriber
-            </div>
-
-            <div class="text-center mb-3 text-secondary text-opacity-75">
-                Subscribe to get the notification of latest posts
-            </div>
-
-            <div class="d-flex justify-content-center align-items-center flex-column">
-                <div class="col-10 mb-3">
-                    <input type="text" name="" class="form-control shadow-none border border-secondary rounded-0" placeholder="Your email address" autocomplete="new-news-letter" required>
+    <div class="modal fade" id="openNewsLetterModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content object-fit-cover img-fluid py-2 px-0" :style="{ background: 'url(' + NewsLetterImageUrl + ')' }">
+                <div class="modal-header d-flex justify-content-end border-bottom-0">
+                    <button type="button" class="btn-close" @click="closeNewsLetter"></button>
                 </div>
-                <div class="col-4 mb-4">
-                    <button type="button" class="btn btn-theme rounded-0 w-100">
-                        Subscribe
-                    </button>
+                <div class="modal-body">
+                    <div class="fs-5 text-center fw-bold mb-3">
+                        Become a subscriber
+                    </div>
+                    <div class="text-center mb-3 text-secondary text-opacity-75">
+                        Subscribe to get the notification of latest posts
+                    </div>
+                    <div class="d-flex justify-content-center align-items-center flex-column">
+                        <div class="col-10 mb-3">
+                            <input type="text" name="" class="form-control shadow-none border border-secondary rounded-0" placeholder="Your email address" autocomplete="new-news-letter" required>
+                        </div>
+                        <div class="col-4 mb-4">
+                            <button type="button" class="btn btn-theme rounded-0 w-100">
+                                Subscribe
+                            </button>
+                        </div>
+                        <label for="checkbox" class="form-label text-secondary text-opacity-75">
+                            <input id="checkbox" type="checkbox" class="form-check-input" name="newLetterPass">
+                            Don't show this popup again
+                        </label>
+                    </div>
                 </div>
-                <label for="checkbox" class="form-label text-secondary text-opacity-75">
-                    <input id="checkbox" type="checkbox" class="form-check-input" name="newLetterPass">
-                    Don't show this popup again
-                </label>
             </div>
-
         </div>
     </div>
 
@@ -67,35 +66,31 @@ export default {
             /* Data properties for the component */
             imageUrl: '/images/dots.png',
             NewsLetterImageUrl: '/images/news-letter.webp',
-            showNewsLetter: false,
         }
 
     },
 
     mounted() {
-        window.addEventListener('scroll', this.handleScroll);
-    },
 
-    beforeDestroy() {
-        window.removeEventListener('scroll', this.handleScroll);
+        setTimeout(() => {
+            this.openNewsLetter()
+        }, 5000);
+
     },
 
     methods: {
 
-        /* Function to check if user has scrolled enough to reveal the news-letter content */
-        handleScroll() {
-
-            if (!this.showNewsLetter && window.scrollY > 100) {
-                this.showNewsLetter = true;
-            }
+        /* Function to open newsLetter */
+        openNewsLetter() {
+            const myModal = new bootstrap.Modal("#openNewsLetterModal", { keyboard: false } );
+            myModal.show();
         },
 
         /* Function to close newsletter */
         closeNewsLetter() {
-            let newsModal = document.querySelector('.news-letter');
-            if (newsModal.classList.contains('news-letter')) {
-                newsModal.classList.add('content-none');
-            }
+            let myModalEl = document.getElementById('openNewsLetterModal');
+            let modal = bootstrap.Modal.getInstance(myModalEl)
+            modal.hide();
         },
 
     }
