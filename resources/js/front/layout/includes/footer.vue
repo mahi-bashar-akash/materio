ce9e941d305d7faa78e9f6acc89744b7b1d1c78f
mahibashardeveloper <mahibashar2023@gmail.com>
2024-04-01 11:43:53 +0600

    update changes

--- resources/js/front/layout/includes/footer.vue
+++ resources/js/front/layout/includes/footer.vue
@@ -86,8 +86,8 @@
             <div class="row row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-2">
                 <div class="p-3">
                     Copyright © 2025
-                    <a href="javascript:void(0)" class="text-theme text-decoration-none">
-                        Materio
+                    <a href="https://redishketch.com/" class="text-theme text-decoration-none">
+                        Redishketch
                     </a>
                     all rights reserved.
                 </div>
